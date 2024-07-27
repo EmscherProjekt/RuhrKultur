06d4d7af814c6dbf3a3f8ad3ab53bfe0296c2951
Rodi <Rodrigo.helwig@proton.me>
2024-07-27 18:44:11 +0200

    feat: Add firstOpen functionality to CacheController

--- lib/app/data/services/dependency_injection.dart
+++ lib/app/data/services/dependency_injection.dart
@@ -1,19 +1,11 @@
 import 'package:ruhrkultur/app/controllers/main_controller.dart';
 import 'package:ruhrkultur/app/controllers/navigation_controller.dart';
-import 'package:ruhrkultur/firebase_options.dart';
-
-
-import 'package:firebase_core/firebase_core.dart';
-
 
 import 'package:get/get.dart';
 
 class DependecyInjection {
   static Future<void> init() async {
 // firebase init
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
 
     Get.put<NavigationController>(NavigationController());
     Get.put<MainController>(MainController());
